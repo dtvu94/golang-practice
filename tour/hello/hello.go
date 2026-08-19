3de43215e4197394fa5cacac7203a855a1b486da
Vu Dang <dtvu94@gmail.com>
2026-08-19 17:01:40 +0700

    Complete the tutorial of module

--- tour/hello/hello.go
+++ tour/hello/hello.go
@@ -14,6 +14,18 @@ func main() {
 
 	fmt.Println(quote.Go())
 
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, errs := greetings.Hellos(names)
+	if errs != nil {
+		log.Fatal(errs)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
+
 	// Get a greeting message and print it.
 	message, err := greetings.Hello("Success")
 
@@ -27,7 +39,7 @@ func main() {
 
 	if errFailed != nil {
 		log.Fatal(errFailed)
+	} else {
+		fmt.Println(messageFailed)
 	}
-
-	fmt.Println(messageFailed)
 }
